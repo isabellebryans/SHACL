057e1646d50cb33b608441f11ef742f8b2d49858
Isabelle Bryans <bryansi@tcd.ie>
2023-03-14 22:30:06 +0100

    Main_RDF4J almost done, just NullPointerException error

--- src/main/java/org/example/Apache/Main_Apache.java
+++ src/main/java/org/example/Apache/Main_Apache.java
@@ -1,26 +1,23 @@
+// Jena takes graph of data and Shapes data type for the shapes
 package org.example.Apache;
 
 import org.apache.jena.graph.Graph;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.shacl.ShaclValidator;
 import org.apache.jena.shacl.Shapes;
 import org.apache.jena.shacl.ValidationReport;
 import org.apache.jena.shacl.lib.ShLib;
-import org.apache.jena.vocabulary.RDF;
+
 import org.example.loadData;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
-import org.apache.jena.shacl.validation.*;
-import org.apache.jena.shacl.vocabulary.SHACL;
+
 
 public class Main_Apache {
 
     public static void main(String[] args) throws FileNotFoundException {
-        System.out.println("Hello world!");
-        Graph dataGraph = loadData.initAndLoadModelFromResource("db_incorrect.ttl", Lang.TURTLE).getGraph();
+        // Load graphs
+        Graph dataGraph = loadData.initAndLoadModelFromResource("db.ttl", Lang.TURTLE).getGraph();
         Graph shapesGraph = loadData.initAndLoadModelFromResource("shapes.ttl", Lang.TURTLE).getGraph();
         Shapes shapes = Shapes.parse(shapesGraph);
 
--- src/main/java/org/example/RDF4J/Main_RDF4J.java
+++ src/main/java/org/example/RDF4J/Main_RDF4J.java
@@ -1,20 +1,67 @@
+// Uses a Reader (set of characters in a stream)
 package org.example.RDF4J;
 
-import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.shacl.ShaclValidator;
+import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.model.vocabulary.RDF4J;
+import org.eclipse.rdf4j.repository.Repository;
+import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.eclipse.rdf4j.repository.RepositoryException;
+import org.eclipse.rdf4j.repository.sail.SailRepository;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.Rio;
+import org.eclipse.rdf4j.sail.memory.MemoryStore;
+import org.eclipse.rdf4j.sail.shacl.ShaclSail;
+import org.eclipse.rdf4j.sail.shacl.ShaclSailValidationException;
+import org.eclipse.rdf4j.sail.shacl.results.ValidationReport;
 import org.example.TopQuadrant.Main_TopQuadrant;
 import org.example.loadData;
 
 
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.*;
 
 public class Main_RDF4J {
+    public static File getFileFromResource(String fileName) {
+        return new File(Main_RDF4J.class.getClassLoader().getResource(fileName).getFile());
+    }
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         System.out.println("Hello world!");
+        ShaclSail shaclSail = new ShaclSail(new MemoryStore());
+        Repository repo = new SailRepository(shaclSail);
+
+        try (RepositoryConnection connection = repo.getConnection()){
+
+            connection.begin();
+
+            // Convert shapes file to a Reader (string of characters)
+            Reader shaclRules = new FileReader(Main_RDF4J.getFileFromResource("shapes.ttl"));
+
+            connection.add(shaclRules, "", RDFFormat.TURTLE, RDF4J.SHACL_SHAPE_GRAPH);
+            connection.commit();
+
+            Reader data = new FileReader(Main_RDF4J.getFileFromResource("db.ttl"));
+            connection.add(data, "", RDFFormat.TURTLE);
+
+            try { connection.commit(); }
+            catch (RepositoryException exception){
+                Throwable cause = exception.getCause();
+                if (cause instanceof ShaclSailValidationException){
+                    System.out.println("SHALC VIOLATION!!!");
+                    ValidationReport report = ((ShaclSailValidationException) cause).getValidationReport();
+                    Model reportModel = ((ShaclSailValidationException) cause).validationReportAsModel();
+                    Rio.write(reportModel, System.out, RDFFormat.TURTLE);
+                } throw exception;
+            }
+
+
+
+        }
+
+
 
     }
 }
