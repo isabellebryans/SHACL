e06b45e91a41c1da3744353e923d1cd3e8dd00f6
Isabelle Bryans <bryansi@tcd.ie>
2023-03-14 13:54:55 +0100

    Main TopQuadrant, Apache and RDF4J files started

---
+++ src/main/java/org/example/Apache/Main_Apache.java
@@ -0,0 +1,18 @@
+package org.example.Apache;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
+import org.example.TopQuadrant.Main_TopQuadrant;
+import org.example.loadData;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class Main_Apache {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println("Hello world!");
+
+    }
+}
---
+++ src/main/java/org/example/RDF4J/Main_RDF4J.java
@@ -0,0 +1,19 @@
+package org.example.RDF4J;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
+import org.example.TopQuadrant.Main_TopQuadrant;
+import org.example.loadData;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class Main_RDF4J {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println("Hello world!");
+
+    }
+}
